5de9657a4adf9571852f3f919cf08f13b60e05af
sohamdev2 <soham@rivuletdigital.com>
2023-01-03 17:56:14 +0530

    ckeditor old build removed

--- ckeditor5_build_old/translations/af.js
+++
@@ -1,1 +0,0 @@
-(function(e){const r=e["af"]=e["af"]||{};r.dictionary=Object.assign(r.dictionary||{},{Bold:"Vetgedruk",Cancel:"Kanselleer",Italic:"Skuinsgedruk","Remove color":"","Remove Format":"Verwyder formatering",Save:"Berg"});r.getPluralForm=function(e){return e!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
